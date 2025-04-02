edbc9a68879f1849d5bf4ed0260dcf0d764d6adf
Brendan <Chermackbrendan@gmail.com>
2025-04-02 16:05:00 -0500

    wrapped mal uploader in a function

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import os, json
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
 
-url = "https://www.virustotal.com/api/v3/files"
 
-payload = { "password": "ia400" }
+def mal_uploader(mal_file_path: str, password=None):
+  load_dotenv()
 
-headers = {
-    "accept": "application/json",
-    "x-apikey": os.getenv("VT_API_KEY")
-}
-with open("./malicious-files/Mal1.zip", "rb") as f:
-    files = { "file": ("Mal1.zip", f, "application/x-zip-compressed") }
-    response = requests.post(url, headers=headers, files=files, data=payload)
+  url = "https://www.virustotal.com/api/v3/files"
 
-print(response.text)
+  payload = { "password": f"{password}" }
 
-data = response.json()
-url = data["data"]["links"]["self"]
+  headers = {
+      "accept": "application/json",
+      "x-apikey": os.getenv("VT_API_KEY")
+  }
+  with open(f"{mal_file_path}", "rb") as f:
+      files = { "file": ("Mal1.zip", f, "application/x-zip-compressed") }
+      response = requests.post(url, headers=headers, files=files, data=payload)
 
-response = requests.get(url, headers=headers)
-print(response.text)
+  print(response.text)
+
+  data = response.json()
+  url = data["data"]["links"]["self"]
+
+  response = requests.get(url, headers=headers)
+  print(response.text)
+  return response.text
+
+res = mal_uploader("./malicious-files/Mal1.zip", "ia400")
+print(res)
